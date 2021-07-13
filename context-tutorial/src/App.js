f1a709078d7fd831ee8de743d30c9f6b1e15584d
오진주 <jinjuoh@hancom.com>
2021-07-14 08:50:29 +0900

    context tutorial

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ColorBox from "./component/ColorBox";
+import { ColorProvider } from './contexts/color'
+import SelectColors from "./component/SelectColors";
+
+const App = () => {
+    return (
+        <ColorProvider>
+            <div>
+                <SelectColors/>
+                <ColorBox/>
+            </div>
+        </ColorProvider>
+    );
+};
+
+export default App;
