b15376a7a1585e026563e1a64de1ab8a628feddc
jinjuoh <jinju7373@naver.com>
2021-10-20 19:18:42 +0900

    24장 프런트엔트 프로젝트:시작 및 회원 인증 구현 - 24장 끝까지

--- blog/blog-frontend/src/setupProxy.js
+++ blog/blog-frontend/src/setupProxy.js
@@ -6,7 +6,8 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://a3d6-175-123-102-30.ngrok.io',
+            // target: 'https://a3d6-175-123-102-30.ngrok.io',
+            target : 'http://localhost:4000',
             changeOrigin: true,
         })
     );
