66f7da6fc6863f00dbd95919d1bb51a83bf0c215
jinjuoh <jinju7373@naver.com>
2021-11-01 17:12:54 +0900

    25장 프론트엔드 프로젝트 : 글쓰기 기능 구현하기

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -4,6 +4,7 @@ import loading from "./loading";
 import {all} from "redux-saga/effects";
 import user, {userSaga} from "./user";
 import write, {writeSaga} from './write';
+import {postSaga} from "./post";
 
 const rootReducer = combineReducers({
     auth,
@@ -13,7 +14,7 @@ const rootReducer = combineReducers({
 });
 
 export function* rootSaga() {
-    yield all([authSaga(), userSaga(), writeSaga()]);
+    yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
 }
 
 export default rootReducer;
